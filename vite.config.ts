355f147ed8246c9c2650c952e59d5613ca213db4
Saeed Kalloub <saeed.kalloub@exalt.ps>
2026-03-29 15:06:14 +0300

    Setup GitHub Pages Action

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react()],
+  base: '/T30/', // أضف هذا السطر بالضبط (اسم المستودع بين سلاشين)
 })
